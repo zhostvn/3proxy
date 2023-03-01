b6144dd7fb18e9b364ae5eb56528fbf865421104
zhostvn <117628532+zhostvn@users.noreply.github.com>
2023-03-01 15:00:55 +0700

    Update setup-non-auth.sh

--- setup-non-auth.sh
+++ setup-non-auth.sh
@@ -86,7 +86,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "ngockieu/doremon/$IP4/$port/$(gen64 $IP6)"
+        echo "$IP4/$port/$(gen64 $IP6)"
     done
 }
 
